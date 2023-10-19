291d448a2243d7cfa4c59482d822b59e4c07c228
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-10-19 21:29:01 +0300

    maxConsecutiveOnes 3

--- src/Arrays/SlidingWindow/Medium/max_consecutive_ones_3.js
+++ src/Arrays/SlidingWindow/Medium/max_consecutive_ones_3.js
@@ -18,3 +18,32 @@
  * @param {number} k
  * @return {number}
  */
+
+// [100101] 2
+export function maxConsecutiveOnes(arr, k) {
+  // Time Complexity: O(n);
+  // Space Complexity: O(1);
+
+  if (k == arr.length) {
+    return arr.length;
+  }
+
+  let countZeros = 0;
+  let ans = 0;
+  let left = 0;
+
+  for (let right = 0; right < arr.length; right++) {
+    if (arr[right] === 0) {
+      countZeros++;
+    }
+
+    while (countZeros > k) {
+      if (arr[left] === 0) {
+        countZeros--;
+      }
+      left++;
+    }
+    ans = Math.max(ans, right - left + 1);
+  }
+  return ans;
+}
--- src/Arrays/SlidingWindow/Medium/tests/max_consecutive_ones_3.test.js
+++ src/Arrays/SlidingWindow/Medium/tests/max_consecutive_ones_3.test.js
@@ -1,5 +1,35 @@
-import {describe} from "vitest";
-
+import {maxConsecutiveOnes} from "../max_consecutive_ones_3";
 describe("Testing 'Max Consecutive Ones III' Algorithm", () => {
-  test("Test 1", () => {});
+  test("Test 1", () => {
+    const arr = [1, 0, 0, 1, 0, 1];
+    const k = 2;
+    const expectedOutput = 4;
+    expect(maxConsecutiveOnes(arr, k)).toEqual(expectedOutput);
+  });
+
+  test("Test 2", () => {
+    const arr = [1];
+    const k = 0;
+    const expectedOutput = 1;
+    expect(maxConsecutiveOnes(arr, k)).toEqual(expectedOutput);
+  });
+  test("Test 3", () => {
+    const arr = [0, 0, 0, 1, 0, 0, 0, 0, 1];
+    const k = 3;
+    const expectedOutput = 4;
+    expect(maxConsecutiveOnes(arr, k)).toEqual(expectedOutput);
+  });
+
+  test("Test 4", () => {
+    const arr = [0, 0, 0, 1, 0, 0, 0, 0, 1];
+    const k = 7;
+    const expectedOutput = arr.length;
+    expect(maxConsecutiveOnes(arr, k)).toEqual(expectedOutput);
+  });
+  test("Test 4", () => {
+    const arr = [0, 0];
+    const k = 2;
+    const expectedOutput = arr.length;
+    expect(maxConsecutiveOnes(arr, k)).toEqual(expectedOutput);
+  });
 });
