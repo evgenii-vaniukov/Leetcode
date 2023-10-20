9541e6f542ddf7834888c54ac8fcf1f2b7b34e62
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-10-20 21:01:46 +0300

    maxConsecutiveOnes

---
+++ src/Arrays/SlidingWindow/max_consecutive_ones/tests/max_consecutive_ones.test.js
@@ -0,0 +1,20 @@
+import {maxConsecutiveOnes} from "../max_consecutive_ones";
+
+describe("Testing 'Max Consecutive Ones' Algorithm", () => {
+  test("Test 1", () => {
+    const arr = [1, 1, 0, 1, 1, 1];
+    const expectedOutput = 3;
+    expect(maxConsecutiveOnes(arr)).toEqual(expectedOutput);
+  });
+
+  test("Test 1", () => {
+    const arr = [0];
+    const expectedOutput = 0;
+    expect(maxConsecutiveOnes(arr)).toEqual(expectedOutput);
+  });
+  test("Test 1", () => {
+    const arr = [0, 1];
+    const expectedOutput = 1;
+    expect(maxConsecutiveOnes(arr)).toEqual(expectedOutput);
+  });
+});
