e9562fbd66f51e72fbab4c370d42cc317a4ddf99
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-10-30 16:31:51 +0200

    twoSum

---
+++ src/Hashing/Easy/TwoSum/two_sum.js
@@ -0,0 +1,44 @@
+// Solution to a LeetCode problem: Two Sum
+
+/**  
+  Topic: Hashing.
+  Approach: Hash Table.
+  Difficulty: Easy.
+*/
+
+// Given an array of integers and an integer target,
+// return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution,
+// and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Find examples in the 'tests' folder.
+
+// Input-Output Format:
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @return {number[]}
+ */
+
+export function twoSum(arr, target) {
+  // Time Complexity: O(n);
+  // Space Complexity: O(n);
+
+  let map = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    let curr = arr[i];
+    let complement = target - curr;
+
+    if (map.has(complement)) {
+      return [
+        [i, map.get(complement)],
+        [map.get(complement), i],
+      ];
+    }
+    map.set(curr, i);
+  }
+  return [-1, -1];
+}
---
+++ src/Hashing/Easy/TwoSum/two_sum.test.js
@@ -0,0 +1,18 @@
+import {twoSum} from "./two_sum";
+describe("Testing 'Two Sum' Algorithm", () => {
+  test("Test 1", () => {
+    const arr = [9, 1, 4];
+    const target = 13;
+    const expectedOutput1 = [2, 0];
+    const expectedOutput2 = [0, 2];
+    expect(twoSum(arr, target)).toEqual([expectedOutput1, expectedOutput2]);
+  });
+  test("Test 2", () => {
+    const arr = [9, 4];
+    const target = 13;
+    const expectedOutput1 = [1, 0];
+    const expectedOutput2 = [0, 1];
+
+    expect(twoSum(arr, target)).toEqual([expectedOutput1, expectedOutput2]);
+  });
+});
